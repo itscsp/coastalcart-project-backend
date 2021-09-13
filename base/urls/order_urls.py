23b2000ce441ad535d534d83584916a03da1ea50
itscsp <cspoojary7972@gmail.com>
2021-09-13 12:45:32 +0530

    completed paypal feature

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -5,4 +5,8 @@ from base.views import order_views as views
 urlpatterns = [
     
     path('add/', views.addOrderItems, name='orders-add'),
-] 
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
+
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
+]  
