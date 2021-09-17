dc4d1109b2a0b9d8c2f4b637aa83dc2ad6b39a60
itscsp <cspoojary7972@gmail.com>
2021-09-17 08:34:54 +0530

    completed all design and logic

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 from base.models import Product, Review
@@ -17,11 +18,33 @@ def getProducts(request):
     query = request.query_params.get('keyword')
     if query == None:
         query = ''
-        
-    products = Product.objects.filter(name__icontains=query)
-    
+
+    products = Product.objects.filter(name__icontains=query).order_by('-createdAt')
+
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 8)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+    print('Page:', page)
+    serializer = ProductSerializer(products, many=True)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
+
+@api_view(['GET'])
+def getTopProducts(request):
+    products = Product.objects.filter(rating__gte=3).order_by('-rating')[0:5]
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
 
 #VIEWS FOR SINGLE PRODUCTS
 @api_view(['GET'])
